db12bfc0c4e3b62b05a73a7d23e702e5038d04ef
MAC_Herb <hewang@mail.bnu.edu.cn>
2019-02-10 17:04:52 +0800

    Add hostby.html and hosts.yaml

--- run.py
+++ run.py
@@ -20,6 +20,7 @@ add_datas = ['./_data/_effective_kaggle.yaml',
             './_data/_effective_kesci.yaml',
             './_data/_effective_dcjingsai.yaml',
             './_data/_effective_datafountain.yaml',
+            './_data/_effective_ICME.yaml',
             './_data/_effective_others.yaml']
 
 competitions = []
@@ -32,7 +33,10 @@ competitions.sort(key=lambda x: x['deadtime'])
 
 app = Flask(__name__)
 
+
 app.config['FREEZER_DESTINATION'] = 'docs'
+# https://stackoverflow.com/questions/11577147/how-to-fix-page-404-on-github-page
+app.config['FREEZER_DESTINATION_IGNORE'] = ['.git*', 'CNAME']
 
 freezer = Freezer(app)
 
@@ -58,6 +62,17 @@ def index(id_type_checkboxs = id_type_checkboxs, id_type2_checkboxs = id_type2_c
     return render_template('index.html', id_type_checkboxs=id_type_checkboxs, 
                                          id_type2_checkboxs=id_type2_checkboxs,
                                          competitions=competitions)
+
+
+with open('./_data/_hosts.yaml', 'r', encoding='utf-8') as f:
+    cfg = f.read()
+    hosts = yaml.load(cfg)
+
+@app.route('/hostby')
+def hostby(id_type_checkboxs = id_type_checkboxs, hosts = hosts):
+
+    return render_template('hostby.html', id_type_checkboxs=id_type_checkboxs, hosts = hosts)
+
 
 if __name__ == '__main__':
     # freezer.freeze()
